dbc8820e2164d2b822b14f036824aece890f1866
Anthony Ciacci <ciacci1234@gmail.com>
2016-07-22 13:47:06 -0400

    adds practice seed files, renames type to category for practices

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160721230223) do
+ActiveRecord::Schema.define(version: 20160722173832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20160721230223) do
 
   create_table "practices", force: :cascade do |t|
     t.string   "name",        null: false
-    t.string   "type",        null: false
+    t.string   "category",    null: false
     t.text     "description", null: false
     t.string   "method",      null: false
     t.datetime "created_at",  null: false

1bfcd3bda692d92d347fd791dd9d4228eb2206ac
Seth Goldman <seth.daniel.goldman@gmail.com>
2016-07-22 12:39:19 -0400

    Adds avatars for all users, styles the users index, and show pages a bit. Moving on to activation.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,13 +47,6 @@ class ApplicationController < ActionController::Base
     !current_user.nil?
   end
 
-  def gravatar_for(user, options = { size: 80 })
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
-  end
-
   private
 
   def is_current_user
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
     not_found if @user != @current_user
     respond_to do |format|
       if @user.update(user_params)
-        flash[:success] = "User was successfully created."
+        flash[:success] = "Profile successfully updated."
         format.html { redirect_to @user, notice: "User was successfully edited." }
         format.json { render :show, status: :created, location: @user }
       else

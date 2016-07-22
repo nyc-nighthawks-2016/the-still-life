d835a45d602cfb2e5716aae2e0d43799145e30e7
Joel_L <plagueoffrogs@gmail.com>
2016-07-22 15:15:44 -0400

    Fix scope bug

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,8 +1,8 @@
 class Resource < ActiveRecord::Base
-	# scope readings:, where(type: 'Reading') 
- #  scope recordings:, where(type: 'Audio') 
- #  scope videos:,  where(type: 'Video') 
- #  scope images:, where(type: 'Image') 
+	scope :readings, -> { where(type: 'Reading') }
+  scope :recordings, -> { where(type: 'Audio') }
+  scope :videos,  -> { where(type: 'Video') }
+  scope :images, -> { where(type: 'Image') }
 
   has_many :bookmarks
 	has_many :users, through: :bookmarks

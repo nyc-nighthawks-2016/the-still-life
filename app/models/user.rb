4ed8b6f4d8c1cc3e8f67a290e3f984c79923867d
Seth Goldman <seth.daniel.goldman@gmail.com>
2016-07-21 20:26:47 -0400

    Still working through the users show page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
   validates :gender, presence: true
   validates_inclusion_of :age, :in => 5..150
+  validates :about_me, length: {maximum: 1000}
+  # has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  # validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   has_secure_password
 
   belongs_to :practice

59e394352108173864b4eb3a20c913cf2b76609d
Seth Goldman <seth.daniel.goldman@gmail.com>
2016-07-22 15:47:12 -0400

    Adds password reset page, begins to check the work flow of it to handle errors.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722183551) do
+ActiveRecord::Schema.define(version: 20160722193509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,8 @@ ActiveRecord::Schema.define(version: 20160722183551) do
     t.string   "activation_digest"
     t.boolean  "activated",           default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   create_table "videos", force: :cascade do |t|

5180d0884f6a2604a092dc31c236d71949f9ea81
Seth Goldman <seth.daniel.goldman@gmail.com>
2016-07-23 17:04:25 -0400

    Adds heroku link to production file.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,15 +65,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  host = 'localhost:3000'
+  host = 'murmuring-savannah-73602.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => '3000',
+    :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'test_app.com',
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 

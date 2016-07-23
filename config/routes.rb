5bfc0034a3614deb5fcdd140a5dfcaf9cf45212d
Seth Daniel Lawson Goldman <seth.daniel.goldman@gmail.com>
2016-07-23 17:29:50 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,18 +11,18 @@ Rails.application.routes.draw do
 
   resources :users
   resources :practices
-<<<<<<< 6e2aece2099d642c03ae0986d34a8958fe70c32b
+
   resources :regimes
   resources :account_activations, only: [:edit]
 
   resources :password_resets, only: [:new, :create, :edit, :update]
-=======
+
   resources :resources
   resources :audios, controller: 'resources', type: 'Audio'
   resources :images, controller: 'resources', type: 'Image'
   resources :readings, controller: 'resources', type: 'Reading'
   resources :videos, controller: 'resources', type: 'Video'
->>>>>>> creates show pages for all resources, general and specific
+
 
   root 'staticpages#index'
 end

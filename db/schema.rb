78f544763a6e6ae616ecbe4819830eeffbbfce84
Conturbo <Conturbodev@gmail.com>
2016-07-23 19:59:21 -0400

    Added text reminders.  Reminders are currently broken, they send a text on regime creation instead of as a reminder.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema.define(version: 20160723201113) do
 
   # These are extensions that must be enabled in order to support this database
@@ -30,6 +29,22 @@ ActiveRecord::Schema.define(version: 20160723201113) do
 
   add_index "bookmarks", ["resource_id"], name: "index_bookmarks_on_resource_id", using: :btree
   add_index "bookmarks", ["user_id"], name: "index_bookmarks_on_user_id", using: :btree
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "images", force: :cascade do |t|
     t.datetime "created_at", null: false

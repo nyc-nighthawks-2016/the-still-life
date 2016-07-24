15265c2fd12508b2c4bed7d3c2e1c8ad524d3308
Seth Goldman <seth.daniel.goldman@gmail.com>
2016-07-23 23:49:50 -0400

    Trying to add pagination for each practices slide.

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -1,9 +1,9 @@
 class PracticesController < ApplicationController
   def index
-    @chants = Practice.chants
-    @body_practices = Practice.body
-    @seated_practices = Practice.seated
-    @services = Practice.service
+    @chants = Practice.chants.paginate(page: params[:page])
+    @body_practices = Practice.body.paginate(page: params[:page])
+    @seated_practices = Practice.seated.paginate(page: params[:page])
+    @services = Practice.service.paginate(page: params[:page])
   end
 
   def show

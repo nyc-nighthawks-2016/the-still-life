38023e16bec1419b6897e1b155bb38c6306b518b
Seth Goldman <seth.daniel.goldman@gmail.com>
2016-07-24 11:22:22 -0400

    To set up fullpage js for each inividual practice. Remove pagination.

--- app/controllers/practices_controller.rb
+++ app/controllers/practices_controller.rb
@@ -1,9 +1,9 @@
 class PracticesController < ApplicationController
   def index
-    @chants = Practice.chants.paginate(page: params[:page], :per_page => 3)
-    @body_practices = Practice.body.paginate(page: params[:page], :per_page => 3)
-    @seated_practices = Practice.seated.paginate(page: params[:page], :per_page => 3)
-    @services = Practice.service.paginate(page: params[:page], :per_page => 3)
+    @chants = Practice.chants
+    @body_practices = Practice.body
+    @seated_practices = Practice.seated
+    @services = Practice.service
   end
 
   def show

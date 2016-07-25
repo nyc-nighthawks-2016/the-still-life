14823f9a6a7e59aae5809b076c47df62e2db9a76
Seth Goldman <seth.daniel.goldman@gmail.com>
2016-07-25 12:35:29 -0400

    Relocates jquery script link before anything else. Removes link to and doctype from practice html page. Moves Fullpage js, and drop down javascript to the application js page.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,89 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+$(document).ready(function() {
+      $('#fullpage').fullpage({
+        sectionsColor: ['#1bbc9b', '#4BBFC3', '#9683ec', '#4BBFC3', '#5bad9f', '#7BAABE', 'whitesmoke', '#ccddff', '#1bbc9b'],
+         anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage', 'seventhPage', 'eighthPage', 'ninthPage'],
+        menu: '#menu',
+        lockAnchors: false,
+        navigation: false,
+        navigationPosition: 'right',
+        navigationTooltips: ['firstSlide', 'secondSlide'],
+        showActiveTooltip: false,
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 1000,
+        autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        // fixedElements: '#header',
+        scrollOverflow: false,
+        scrollOverflowOptions: null,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+        bigSectionsDestination: null,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+
+        // paddingTop: '3em',
+        // paddingBottom: '10px',
+
+        responsiveWidth: 0,
+        responsiveHeight: 0,
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        //events
+        onLeave: function(index, nextIndex, direction){
+          //leaving first section
+          if(index == 1) {
+            $('#header').addClass('fixed');
+          }
+          //back to the first section
+          // if(nextIndex == 0) {
+          //   $('#header').removeClass('fixed');
+          // }
+        },
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+
+      });
+
+    // ADD SLIDEDOWN ANIMATION TO DROPDOWN //
+	  $('.dropdown').on('show.bs.dropdown', function(e){
+	    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+	  });
+
+	  // ADD SLIDEUP ANIMATION TO DROPDOWN //
+	  $('.dropdown').on('hide.bs.dropdown', function(e){
+	    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+	  });
+      
+
+    });
+

a4fdb86454ef86a1b11c5bdb0211c7adde6cbcfc
Seth Goldman <seth.daniel.goldman@gmail.com>
2016-07-25 15:38:59 -0400

    Adds back h4 tag with class panel title to all drop downs, just to stick with tutorial convention.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -71,15 +71,7 @@ $(document).ready(function() {
 
       });
 
-    // ADD SLIDEDOWN ANIMATION TO DROPDOWN //
-	  $('.dropdown').on('show.bs.dropdown', function(e){
-	    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
-	  });
-
-	  // ADD SLIDEUP ANIMATION TO DROPDOWN //
-	  $('.dropdown').on('hide.bs.dropdown', function(e){
-	    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
-	  });
+    
       
 
     });

126606ed78320dce571aa21b7fb94053d81590b1
Seth Goldman <seth.daniel.goldman@gmail.com>
2016-07-25 21:45:18 -0400

    Updates Regimen edit forms, and controller to update the time for a users current regime.

--- app/helpers/regimes_helper.rb
+++ app/helpers/regimes_helper.rb
@@ -1,6 +1,6 @@
 module RegimesHelper
 	def display_time(regime)
-		(regime.daily_practice_time - 4.hours).strftime("%b %e, %l : %M %P")
+		(regime.daily_practice_time).strftime("%b %e, %l : %M %P")
 		# regime.daily_practice_time
 	end
 end
